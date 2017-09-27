71a3299dc23bae342a6b3e398bfeac49328282ee
Zyue10969 <ying_hui104@163.com>
2017-09-27 18:04:25 +0800

    Create ContainerWithMostWater.py

---
+++ ContainerWithMostWater.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        max_water = 0
+        if len(height) < 2:
+            exit("Error: length of height is at least 2.")
+        for i in range(len(height)):
+            ai = height[i]
+            for j in range(i + 1, len(height)):
+                aj = height[j]
+                max_water = max(max_water, (j - i) * (min(ai, aj) + 0.5 * abs(aj - ai)))
+        return max_water
+        
+                
