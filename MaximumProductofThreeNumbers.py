f64ed900f28b324478e50ee4e55a249dc7f24ba9
Zyue10969 <ying_hui104@163.com>
2017-10-03 16:04:17 +0800

    Update MaximumProductofThreeNumbers.py

--- MaximumProductofThreeNumbers.py
+++ MaximumProductofThreeNumbers.py
@@ -1,3 +1,4 @@
+import heapq
 class Solution(object):
     def maximumProduct(self, nums):
         """
@@ -5,5 +6,11 @@ class Solution(object):
         :rtype: int
         """
         #结论:三个数的乘积的最大值 = max(min1 * min2 * max, max1 * max2 * max3)
+        #思路一：排序后获得TopN值
         nums.sort()
         return max(nums[0] * nums[1] * nums[-1], nums[-1] * nums[-2] * nums[-3])
+    
+        #思路二：用heapq堆数据结构获得TopN值
+        min2 = heapq.nsmallest(2, nums)
+        max3 = heapq.nlargest(3, nums)
+        return max(min2[0] * min2[1] * max3[2], max3[0] * max3[1] * max3[2])
