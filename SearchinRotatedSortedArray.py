1b37f6816589d9a9a2871856de697d7f031fb7b6
Zyue10969 <ying_hui104@163.com>
2017-10-05 16:11:53 +0800

    Create SearchinRotatedSortedArray.py

---
+++ SearchinRotatedSortedArray.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        #思路:二分查找
+        if not nums:
+            return -1
+        low = 0
+        high = len(nums) - 1
+        while low <= high:
+            mid = (low + high) / 2
+            if nums[mid] == target:
+                return mid
+#             if nums[low] == target:
+#                 return low
+#             if nums[high] == target:
+#                 return high
+            
+            if nums[low] <= nums[mid]:
+                if nums[low] <= target < nums[mid]:
+                    high = mid - 1
+                else:
+                    low = mid + 1
+            else:
+                if nums[mid] < target <= nums[high]:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+        return -1
